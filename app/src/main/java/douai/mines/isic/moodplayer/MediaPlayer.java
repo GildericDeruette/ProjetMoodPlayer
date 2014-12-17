dd6b2ca75da2e233afcf162be2dd2e1ee95816af
zhimin zhou <zzmax.zhou@gmail.com>
2014-12-17 12:23:20 +0100

    ADD Interface

---
+++ app/src/main/java/douai/mines/isic/moodplayer/MediaPlayer.java
@@ -0,0 +1,10 @@
+package douai.mines.isic.moodplayer;
+
+/**
+ * Created by zzmax_000 on 17/12/2014.
+ */
+public class MediaPlayer {
+    private Button play;
+    private Button  pause;
+    private Button next;
+}
